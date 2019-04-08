1bc48282af8198878f34dff8aa611ec98e1dbc84
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-08 12:35:54 -0400

    Cleaned up GenericUtils making use of Guava commons

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,9 +1,7 @@
 package rocks.zipcode.io.quiz4.generics;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
 
@@ -12,45 +10,27 @@ import com.google.common.collect.Sets;
  */
 public class GenericUtils {
     public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
-        _ firstElement = originalSet.iterator().next();
-        List<Set<_>> sets = new ArrayList<>(actualPowerSet(originalSet));
+        List<Set<_>> sets = new ArrayList<>(Sets.powerSet(originalSet));
         Set<Set<_>> newSets = new LinkedHashSet<>();
-        int longest = sets.get(0).size();
-        sets = sets.stream().filter(s -> s.size() < longest).collect(Collectors.toList());
+        int longest = originalSet.size();
         for (Set<_> set : sets) {
             if (set.size() < longest && set.size() > 1) {
-                Set newSet = new LinkedHashSet();
-                Iterator<_> iterator =  set.iterator();
-                _ second = iterator.next();
-                _ first = iterator.next();
-                newSet.add(first);
-                newSet.add(second);
-                newSets.add(newSet);
+                newSets.add(swapValues(set));
             }
         }
         sets.addAll(newSets);
-        sets.add(originalSet);
         Collections.sort(sets, new SortSets<>());
         return sets;
     }
 
-    public static <_ extends Comparable> List<? extends Set<_>> actualPowerSet(Set<_> originalSet) {
-        List<Set<_>> sets = new ArrayList<>();
-        if (originalSet.isEmpty()) {
-            sets.add(new LinkedHashSet<_>());
-            return sets;
-        }
-        List<_> list = new ArrayList<_>(originalSet);
-        _ head = list.get(0);
-        Set<_> rest = new HashSet<_>(list.subList(1, list.size()));
-        for (Iterable<_> set : actualPowerSet(rest)) {
-            Set<_> newSet = new LinkedHashSet<_>();
-            newSet.add(head);
-            newSet.addAll((Collection<? extends _>) set);
-            sets.add(newSet);
-            sets.add((LinkedHashSet<_>) set);
-        }
-        return sets;
+    private static <_ extends Comparable> Set swapValues(Set<_> set) {
+        Set<_> newSet = new LinkedHashSet();
+        Iterator<_> iterator =  set.iterator();
+        _ second = iterator.next();
+        _ first = iterator.next();
+        newSet.add(first);
+        newSet.add(second);
+        return newSet;
     }
 
     public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(_... originalSet) {
